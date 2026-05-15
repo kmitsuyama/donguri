bee1993c9ecebb3124181ad1ce133dbd800c7c91
kmitsuyama <kazuya-mitsuyama@navio.co.jp>
2026-05-15 09:23:05 +0900

    Enter後に入力欄をクリアする

--- donguri_db_st.py
+++ donguri_db_st.py
@@ -5,6 +5,7 @@ import sys
 import os
 import re
 import sqlite3
+from datetime import datetime
 from googletrans import Translator
 import firebase_admin
 from firebase_admin import credentials
@@ -217,6 +218,10 @@ def transKana(doc, db):
 def clear_text():
     st.session_state['input_text'] = ''
 
+def clear_history():
+    st.session_state['conversion_history'] = []
+    st.session_state['last_conversion'] = None
+
 translator = Translator()
 
 if not firebase_admin._apps:
@@ -226,13 +231,45 @@ db = firestore.client()
 
 if 'input_text' not in st.session_state:
     st.session_state['input_text'] = ''
+if 'conversion_history' not in st.session_state:
+    st.session_state['conversion_history'] = []
+if 'last_conversion' not in st.session_state:
+    st.session_state['last_conversion'] = None
+
+def convert_input_text():
+    src = st.session_state['input_text']
+    if src == '':
+        return
+
+    des = translator.translate(src, dest='en')
+    kana_text = transKana(des.text, db)
+    conversion = {
+        '時刻': datetime.now().strftime('%H:%M:%S'),
+        '入力': src,
+        '英語': des.text,
+        'かな読み': kana_text,
+    }
+    st.session_state['last_conversion'] = conversion
+    st.session_state['conversion_history'].insert(0, conversion)
+    st.session_state['input_text'] = ''
     
 st.title("どんぐり変換")
-src=st.text_input('ここ(▼▼)に変換する日本語を入れてください', key='input_text')
+st.text_input(
+    'ここ(▼▼)に変換する日本語を入れてください',
+    key='input_text',
+    on_change=convert_input_text,
+)
 st.button('消去', on_click=clear_text)
-if src != '':
-    des = translator.translate(src, dest='en')
-    '英語では　：',des.text
-    'かな読みは：',transKana(des.text, db)
+if st.session_state['last_conversion']:
+    '英語では　：',st.session_state['last_conversion']['英語']
+    'かな読みは：',st.session_state['last_conversion']['かな読み']
+
+st.subheader('変換履歴')
+st.caption('この履歴は現在接続しているセッション内だけ保持されます。')
+st.button('履歴を消去', on_click=clear_history)
+if st.session_state['conversion_history']:
+    st.dataframe(st.session_state['conversion_history'], use_container_width=True)
+else:
+    st.info('まだ変換履歴はありません。')
 
 #EOF
